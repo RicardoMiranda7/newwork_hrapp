f2e1e87c201a93d23335df1b6908a0b603738482
Ricardo Miranda <ricardo.miranda704@gmail.com>
2025-11-19 09:43:17 +0200

    Add new models to admin page

--- backend/hrapp/admin.py
+++ backend/hrapp/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 
-from .models import User, Profile, Feedback, AbsenceRequest
+from .models import User, Profile, Feedback, AbsenceRequest, AbsenceLedger, \
+    BankHoliday
 
 # All models are registered for admin interface
 admin.site.register(User)
 admin.site.register(Profile)
 admin.site.register(Feedback)
 admin.site.register(AbsenceRequest)
+admin.site.register(AbsenceLedger)
+admin.site.register(BankHoliday)
